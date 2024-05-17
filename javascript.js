8fe9f2041871f672328b20a47d2cca2c8fdcc15d
MarceloFilho1 <marcelofcordova@hotmail.com>
2024-05-16 21:29:12 -0300

    1º commit - ativiade

---
+++ javascript.js
@@ -0,0 +1,23 @@
+var vetValues = ["Luis", 123]
+
+
+for (var i = 0; i < 2; i++) {
+    console.log(vetValues[i])
+}
+
+
+let nome
+let idade
+
+nome = prompt("Informe um nome: ")
+alert(nome)
+
+idade = prompt("Informe a idade: ")
+alert(idade)
+
+if (idade >= 18) {
+    alert("Maior de idade!")
+}
+else {
+    alert("Menor de idade!")
+}
