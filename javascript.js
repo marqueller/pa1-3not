a6c6579a7463e56684d20592945a1c11dddb673c
luankuhnen <luankuhnen22@gmail.com>
2024-06-07 19:49:41 -0300

    2 commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var vetValues = ["Luís", 123]
+
+
+for(var i = o; i < 2; i++) {
+    console.log(vetValues[i])
+}
+
+let nome 
+let idade
+
+nome = prompt("Informe um nome: ")
+alert(nome)
+
+idade = promp("Informe a idade: ")
+alert(idade)
+
+if(idade >= 18){
+    alert ("maior de idade!")
+}
+else{
+    alert("menor de idade!")
+}   
