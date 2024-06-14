1e7deba048b87e76f034b2252fc461a13b0929fa
Jeninin <jennifernetopadilha@gmail.com>
2024-06-14 20:02:02 -0300

    1° commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> MJFlix &copy; Desenvolvido por "Maisa e Jennifer" 2024</h2>
+        </footer>
+    );
+}
+export default Footer;
